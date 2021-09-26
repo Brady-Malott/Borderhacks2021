6f29cb950c20222eaa7621299967d27f77e021e4
Thermalrex <yusuf.naeb@outlook.com>
2021-09-25 20:54:10 -0400

    traffic score calculations done, and now api receiving data working

--- src/services/data.py
+++ src/services/data.py
@@ -1,69 +1,74 @@
+from typing import Match
 import requests
 import warnings
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 import pandas
 
-LIGHT_SCORE = 1  # light, motorcycle
-SINGLE_UNIT_SCORE = 2  # single unit
-BUS_SCORE = 3  # bus, articulated truck
+BICYCLE_SCORE = 1
+LIGHT_SCORE = 2
+SINGLE_UNIT_SCORE = 3
+BUS_SCORE = 4
+ARTICILATED_TRUCKS = 5
 
 warnings.simplefilter('ignore', InsecureRequestWarning)  # ignore that stupid stinky smelling warning
 
-year = 2021
-month = 9
-day = 24
-intersectionId = 1
-start_hour = 14  # uses 24 hour time
-start_minute = 20
-end_hour = 14  # uses 24 hour time
-end_minute = 21
+
 to_bridge = True
 bridge_dir = 'N'
-traffic_score = 0
 
-light_total = 0
-single_unit_total = 0
-bus_total = 0
+#Calculating the traffic score function
+def traffic_score_calc(vehicle, quantity):
+    if vehicle == 'Bicycle':
+        return (BICYCLE_SCORE * quantity)
+    elif vehicle == 'Light':
+        return (LIGHT_SCORE* quantity)
+    elif vehicle == 'SingleUnitTruck':
+        return (SINGLE_UNIT_SCORE* quantity)
+    elif vehicle == 'Bus':
+        return (BUS_SCORE * quantity)
+    elif vehicle == 'ArticulatedTruck':
+        return (ARTICILATED_TRUCKS * quantity)
+    else:   
+        print(f'!!!vehicleType: {vehicle} was not listed!!!')
 
-link = f"https://opendata.citywindsor.ca/api/traffic?date={year}-{month}-{day}&intersectionId={intersectionId}&start_time={start_hour}%3A{start_minute}&end_time={end_hour}%3A{end_minute}"
-response = requests.get(link, verify=False)
-temp_response = response.json()
+def _set_date_and_time():
+    #Variables for time the api request
+    year = 2021
+    month = 9
+    day = 24
+    intersectionId = 1
+    start_hour = 14  # uses 24 hour time
+    start_minute = 28
+    end_hour = 14  # uses 24 hour time
+    end_minute = 35
+    link = f"https://opendata.citywindsor.ca/api/traffic?date={year}-{month}-{day}&intersectionId={intersectionId}&start_time={start_hour}%3A{start_minute}&end_time={end_hour}%3A{end_minute}"
+    response = requests.get(link, verify=False)
+    temp_response = response.json()
+    print(f"This is the data for {temp_response['intersectionDescription']}:")
+    return temp_response
 
-print(f"This is the data for {temp_response['intersectionDescription']}:")
+def _get_traffic_score (intersectionId):  
+    temp_response = _set_date_and_time()
+    traffic_score = 0
+    score = 0  
+    for value in temp_response["traffic"]:
+        #Vehicle information
+         
+        exit = value['exit']
+        vehicleType = value['vehicleType']
+        qty = value['qty']
+        
+        if exit == 'N':
+                 score = traffic_score_calc(vehicleType, qty)
+        elif exit == 'W':
+                 score = traffic_score_calc(vehicleType, qty)
+        elif exit == 'S':
+                 score = traffic_score_calc(vehicleType, qty)
+        elif exit == 'E':
+                 score = traffic_score_calc(vehicleType, qty)
+        traffic_score += score
+        
+        
+    return traffic_score
 
-
-def set_date_time(_year, _month, _day, _start_hour, _start_minute, _end_hour, _end_minute):
-    year = _year
-    month = _month
-    day = _day
-    start_hour = _start_hour
-    start_minute = _start_minute
-    end_hour = _end_hour
-    end_minute = _end_minute
-
-
-def _get_traffic_score(intersection_id):
-
-
-
-for value in temp_response["traffic"]:
-    entrance = value['entrance']
-    exit = value['exit']
-    vehicleType = value['vehicleType']
-    qty = value['qty']
-    if entrance == 'N' or exit == 'N':
-        if vehicleType == 'Light' or vehicleType == 'Bicycle':
-            traffic_score += LIGHT_SCORE * qty
-            light_total += qty
-        elif vehicleType == 'SingleUnitTruck':
-            traffic_score += SINGLE_UNIT_SCORE * qty
-            single_unit_total += qty
-        elif vehicleType == 'Bus' or vehicleType == 'ArticulatedTruck':
-            traffic_score += BUS_SCORE * qty
-            bus_total += qty
-        else:
-            print(f'!!!vehicleType: {vehicleType} was not listed!!!')
-    #print(f"At {value['timeStamp']}, {value['qty']} vehicles of type {value['vehicleType']} crossed from {value['entrance']} to {value['exit']}")
-
-print(f"There were {light_total} light vehicle(s), {single_unit_total} single unit vehicle(s), and {bus_total} articulated truck(s)")
-print(f"The traffic score is {traffic_score}")
+print(_get_traffic_score(1))
