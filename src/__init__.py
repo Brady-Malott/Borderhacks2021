6293ed0d635f1ffde2ec39b65445748847167e21
Brady-Malott <brady.malott@gmail.com>
2021-09-26 05:53:07 -0400

    More deployment setup

--- src/__init__.py
+++ src/__init__.py
@@ -1,30 +1,19 @@
 import os
 
 from flask import Flask
+from config import Config
 
-def create_app(test_config=None):
-    # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-    )
+# create and configure the app
+app = Flask(__name__, instance_relative_config=True)
+app.config.from_object(Config)
 
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
+# ensure the instance folder exists
+try:
+    os.makedirs(app.instance_path)
+except OSError:
+    pass
 
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
-
-    # Define the blueprints
-    from . import (form, visualizer)
-    app.register_blueprint(form.bp)
-    app.register_blueprint(visualizer.bp)
-
-    return app
+# Define the blueprints
+from . import (form, visualizer)
+app.register_blueprint(form.bp)
+app.register_blueprint(visualizer.bp)
