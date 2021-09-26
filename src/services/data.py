753c9f09c3e47ce4adec246d3a5ca164983c0232
neufee2 <neufee2@mcmaster.ca>
2021-09-26 00:29:42 -0400

    added vehicles, removed errant code

--- src/services/data.py
+++ src/services/data.py
@@ -41,18 +41,13 @@ from flask import Flask, request, render_template
 app = Flask(__name__)
 
 def _set_date_and_time():
-    #Variables for time the api request
-    form_info = dict
-    @app.route('/form_input', methods=['GET','POST'])
-    def form_in():
-        form_info = request.form["/post_field"]
-    year = 2021
-    month = 9
+    year = 2019
+    month = 8
     day = 24
     intersectionId = 1
-    start_hour = 14  # uses 24 hour time
+    start_hour = 10 # uses 24 hour time
     start_minute = 28 # should be user input, not static
-    end_hour = 14  # uses 24 hour time
+    end_hour = 22  # uses 24 hour time
     if (start_minute < 10):
         end_minute = start_minute + 10
     else: 
