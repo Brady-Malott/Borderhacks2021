9ff40e38c4ff591b8c5a2e0a86b9b4bb29d2caff
Brady-Malott <brady.malott@gmail.com>
2021-09-26 01:22:03 -0400

    Linked frontend to backend, visualizer blueprint receives traffic score for 3 intersections

--- src/services/data.py
+++ src/services/data.py
@@ -2,7 +2,7 @@ from typing import Match
 import requests
 import warnings
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
-import pandas
+from datetime import datetime, time, date
 
 BICYCLE_SCORE = 1
 LIGHT_SCORE = 2
@@ -36,52 +36,80 @@ def traffic_score_calc(vehicle, quantity):
 from flask import Flask, request, render_template 
 app = Flask(__name__)
 
-def _set_date_and_time():
-    #Variables for time the api request
-    form_info = dict
-    @app.route('/form_input', methods=['GET','POST'])
-    def form_in():
-        form_info = request.form["/post_field"]
-    year = 2021
-    month = 9
-    day = 24
-    intersectionId = 1
-    start_hour = 14  # uses 24 hour time
-    start_minute = 28 # should be user input, not static
-    end_hour = 14  # uses 24 hour time
-    if (start_minute < 10):
+# Utility function for fixing # hours/minutes < 10
+# eg: Start time for 3:05 is 03:05
+def _get_formatted_time_string(time):
+    if time < 10:
+        return f"0{time}"
+    return f"{time}"
+
+
+def _query_traffic_data(year, month, day, start_hour, start_minute):
+
+    # First, setup the start and end minutes (10 minutes apart) 
+    end_minute = None
+    if start_minute < 10:
         end_minute = start_minute + 10
     else: 
         end_minute, start_minute = start_minute, start_minute - 10
 
-    link = f"https://opendata.citywindsor.ca/api/traffic?date={year}-{month}-{day}&intersectionId={intersectionId}&start_time={start_hour}%3A{start_minute}&end_time={end_hour}%3A{end_minute}"
-    response = requests.get(link, verify=False)
-    temp_response = response.json()
-    print(f"This is the data for {temp_response['intersectionDescription']}:")
-    return temp_response
+    # Fix the start_time strings
+    start_hour_str = _get_formatted_time_string(start_hour)
+    start_minute_str = _get_formatted_time_string(start_minute)
+    end_hour_str = start_hour_str
+    end_minute_str = _get_formatted_time_string(end_minute)
 
-def _get_traffic_score (intersectionId):  
-    temp_response = _set_date_and_time()
+    # Create the finalized start and end time strings
+    start_time = f"{start_hour_str}:{start_minute_str}"
+    end_time = f"{end_hour_str}:{end_minute_str}"
+
+    # Store the traffic results for the 3 intersections
+    traffic_data = []
+    for intersection_id in range(1, 4):
+        link = f"https://opendata.citywindsor.ca/api/traffic?date={year}-{month}-{day}&intersectionId={intersection_id}&start_time={start_time}&end_time={end_time}"
+        response = requests.get(link, verify=False)
+        traffic_array = response.json().get('traffic')
+        traffic_data.append(traffic_array)
+    
+    return traffic_data
+
+def _get_traffic_score (direction_headed, date):
+
+    date = datetime.fromisoformat(date)
+    year = date.year
+    month = date.month
+    day = date.day
+    hour = date.hour
+    minute = date.minute
+    print(year, month, day, hour, minute)
+
+    traffic_data = _query_traffic_data(year, month, day, hour, minute)
+    print(traffic_data)
+
+    intersection_scores = []
+    # Get the traffic score for each intersection, and append each score to the intersection_scores list
+    i = 1
+    for intersection_data in traffic_data:
+        traffic_score = _get_intersection_traffic_score(intersection_data, direction_headed)
+        intersection_scores.append(traffic_score)
+        print(f"Traffic score for intersection {i} is {traffic_score}")
+        i += 1
+        
+    return intersection_scores
+
+def _get_intersection_traffic_score(intersection_data, direction_headed):
     traffic_score = 0
-    score = 0  
-    for value in temp_response["traffic"]:
-        #Vehicle information
-         
-        exit = value['exit']
-        vehicleType = value['vehicleType']
-        qty = value['qty']
+
+    for item in intersection_data:
+        # Vehicle information
+        exit_direction = item['exit']
+        vehicle_type = item['vehicleType']
+        qty = item['qty']
         
-        if exit == 'N':
-                 score = traffic_score_calc(vehicleType, qty)
-        elif exit == 'W':
-                 score = traffic_score_calc(vehicleType, qty)
-        elif exit == 'S':
-                 score = traffic_score_calc(vehicleType, qty)
-        elif exit == 'E':
-                 score = traffic_score_calc(vehicleType, qty)
-        traffic_score += score
-        
+        if exit_direction == direction_headed:
+            traffic_score += traffic_score_calc(vehicle_type, qty)     
         
     return traffic_score
 
-print(_get_traffic_score(1))
+
+print(_get_traffic_score("N", '2021-09-23T17:30:00'))
--- src/visualizer.py
+++ src/visualizer.py
@@ -6,4 +6,7 @@ bp = Blueprint('visualizer', __name__, url_prefix='/visualizer')
 
 @bp.route('/', methods=['GET'])
 def visualizer():
+    print("Visualizer - The traffic scores are:")
+    for score in session['intersection_scores']:
+      print(score)
     return render_template('visualizer/visualizer.html')
